22502ec30bccf8873f41b344f1bddb51777f6e08
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-14 22:18:39 +0530

    Update footer.jsx

--- Frontend/src/components/footer.jsx
+++ Frontend/src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer-exclude mt-20 pb-8'>
       <div className='flex justify-center'>
-        <img className='h-10 w-10 rounded-full cursor-pointer' src="public\DSC01612.JPG" alt="" />
+        <img className='h-10 w-10 rounded-full cursor-pointer' src="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/DSC01612.JPG?raw=true" alt="" />
         <h4 className='bold text-white font-serif pt-2'>&nbsp;&nbsp;Dhruv Mali</h4>
       </div>
     </div>
