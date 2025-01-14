66599490cf9c0d7ab22fc2faa23b1cd8e44f6d47
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-14 21:36:08 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import Body from './components/body'
-import Myjourney from './components/myjourney'
-import Certificates from './components/certificates'
+// import React from 'react'
+// import Navbar from './components/navbar'
+// import Body from './components/body'
+// import Myjourney from './components/myjourney'
+// import Certificates from './components/certificates'
+// import Footer from './components/footer'
+// import About from './components/About/about'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Router>
+//         <Navbar></Navbar>
+//         <Routes>
+//           <Route path='/' element={App}></Route>
+//           <Route path='/about' element={<About></About>}></Route>
+//         </Routes>
+//       </Router>
+//       <Body></Body>
+//       <Myjourney></Myjourney>
+//       <Certificates></Certificates>
+//       <Footer></Footer>
+//     </div>
+//   )
+// }
+
+// export default App
+import React from 'react';
+import Navbar from './components/navbar';
+import Body from './components/body';
+import Myjourney from './components/myjourney';
+import Certificates from './components/certificates';
+import Footer from './components/footer';
+import About from './components/About/about';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Body></Body>
-      <Myjourney></Myjourney>
-      <Certificates></Certificates>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/"  element={<><Body /><Myjourney /><Certificates /><Footer /></>}/>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
