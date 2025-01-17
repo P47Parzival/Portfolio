69ba76aa2c62f9c137db6197af49506a70442940
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-17 17:51:29 +0530

    Update certificates.jsx

--- Frontend/src/components/certificates.jsx
+++ Frontend/src/components/certificates.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import Cardcerti from './certi_card'
-import styled from 'styled-components'
+
 
 const Certificates = () => {
   return (
-    <section>
+    <section className='bg-black'>
       <div className='flex justify-center mt-32'>
         <h1 className='text-4xl bold text-cyan-500 font-serif'>My&nbsp;</h1>
         <h1 className='text-4xl bold text-white font-serif'>Certificates</h1>
       </div>
       <div className='grid-container mt-14 pl-20' style={myStyle}>
-        <Cardcerti image="src\assets\sql_basic certificate_page-0001.jpg" Card_title="HackerRank SQL Certificate"
+        <Cardcerti image="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/sql_basic%20certificate_page-0001.jpg?raw=true" Card_title="HackerRank SQL Certificate"
         Description="Recently Earned an SQL Knowledge Certificate and even got 5⭐ Badge on Hackerrank for solving Basic to Advance level quetions">
         </Cardcerti>
-        <Cardcerti image="src\assets\Lets updgrade node js certificate_page-0001.jpg" Card_title="MERN Stack Certificate"
+        <Cardcerti image="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/Lets%20updgrade%20node%20js%20certificate_page-0001.jpg?raw=true" Card_title="MERN Stack Certificate"
         Description="Recently got and opportunity to attend a bootcamp from let's upgrade which helped me advance my knowledge about MERN stack">
         </Cardcerti>
-        <Cardcerti image="src\assets\tic_tac_toe.jpg" Card_title="Hackathon Certificate"
+        <Cardcerti image="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/tic_tac_toe.jpg?raw=true" Card_title="Hackathon Certificate"
         Description="Gained Knowledge and experience afer attending a 36 hour in-person hackathon at DAIICT">
         </Cardcerti>
-        <Cardcerti image="src\assets\tata_imagination.jpg" Card_title="TATA Imagination Quiz"
+        <Cardcerti image="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/tata_imagination.jpg?raw=true" Card_title="TATA Imagination Quiz"
         Description="TATA imagination quiz organized by tata foundation">
+        </Cardcerti>
+        <Cardcerti image="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/thinkbiz_tecahthon.jpg?raw=true" Card_title="Thinkbiz Teachathon"
+        Description="Thinkbiz 24 hour in-person Techathon 2025">
         </Cardcerti>
       </div>
     </section>
