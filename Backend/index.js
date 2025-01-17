fe849b62dc9c2818052499b1f77555aef7a65ba6
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-17 17:16:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require('body-parser')
 const Client = require('./user')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/postfolio_contacts'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost:27017/postfolio_contacts')
+mongoose.connect(MONGODB_URI)
 .then(() => console.log("mongodb connected"))
 
 app.post('/api/contact', async(req, res) => {
@@ -25,6 +31,6 @@ app.post('/api/contact', async(req, res) => {
     }
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("app is listneing on port 5000")
 })
