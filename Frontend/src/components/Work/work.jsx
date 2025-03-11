fd5b3d766c0dac5c0c82b04d945b44623a62c279
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-03-11 15:46:46 +0530

    Update work.jsx

--- Frontend/src/components/Work/work.jsx
+++ Frontend/src/components/Work/work.jsx
@@ -12,6 +12,18 @@ const Work = () => {
                     imagesource="https://github.com/P47Parzival/Portfolio/blob/main/Frontend/src/assets/blackhole.png?raw=true" 
                 />
                 <Workcard 
+                    sitelink="https://github.com/P47Parzival/UniEase-ERP?tab=readme-ov-file/" 
+                    pcontent="ERP for small tutions and institutes built on top of MERN stack" 
+                    appname="Uni-Ease ERP" 
+                    imagesource="https://raw.githubusercontent.com/P47Parzival/UniEase-ERP/refs/heads/main/frontend/Screenshots/Screenshot%202025-02-18%20222600.png" 
+                />
+                <Workcard 
+                    sitelink="https://github.com/P47Parzival/FunkyCursor/" 
+                    pcontent="Enhance you surfing experience with custom cursor" 
+                    appname="Funky Cursor" 
+                    imagesource="https://raw.githubusercontent.com/P47Parzival/FunkyCursor/refs/heads/main/image.png" 
+                />
+                <Workcard 
                     sitelink="#" 
                     pcontent="My portfolio which you are currently on" 
                     appname="Portfolio" 
