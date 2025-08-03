6b94b23ad490437e83d8a7e8d09f807686004f5f
P47Parzival <dhruvmali999@gmail.com>
2025-08-03 21:44:38 +0530

    new portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,175 @@
+export const theme = "#ff0000" // red
+
+export const name = "Dhruv Mali";
+
+export const aboutMe =
+  "I am a MERN, Python stack developer and AI enthusiast actively working on ML/DL projects to gain hands-on experience. I’ve built everything from Chrome extensions to full-stack academic ERP systems and am deeply involved in building practical AI applications, including language-learning tools, interview simulators, and satellite imagery analysis tools. Currently interning in Generative AI, I’m exploring both research and real-world deployment of machine learning systems.";
+
+export const education1 = {
+  name: "MB Patel Secondary and Higher Secondary School",
+  branch: "10+2",
+  year: "2021-2023", // Update if different
+};
+
+export const education2 = {
+  name: "Adani University",
+  branch: "CSE(AIML)",
+  year: "2023 - 2027", // Update if needed
+};
+
+export const experience = {
+  name: "Generative AI Intern",
+  moreInfo:
+    "Currently worked as an intern in the field of Generative AI, exploring applied projects in ML/DL, particularly around language models, AI agents, and productivity-focused applications.",
+  year: "2025",
+};
+
+export const projects = [
+  {
+    name: "Susap – GD & Interview Skill Builder",
+    description:
+      "Stack: Next.js, Firebase, Socket.io, VAPI, Stripe, AI APIs. A real-time platform to simulate Group Discussions and Interviews with evaluation and feedback.",
+    href: "https://susap.vercel.app",
+    cta: "Learn more",
+    background: "https://cdn-business.nikkei.com/atcl/gen/19/00625/040200007/s1280.jpg?__scale=w:1280,h:720&_sh=0807a01e05",
+    className:
+      "md:col-start-1 md:col-end-4 md:row-start-3 md:row-end-5 col-start-2 col-end-3 row-start-1 row-end-4 ",
+  },
+  {
+    name: "Academic ERP System",
+    description:
+      "Stack: MERN, MongoDB, JWT, AI APIs. Includes attendance, complaint management, academic calendar, secure file sharing, AI-powered Q&A features.",
+    href: "https://github.com/P47Parzival/UniEase-ERP", // replace if repo link differs
+    cta: "Learn more",
+    background: "https://github.com/P47Parzival/UniEase-ERP/blob/main/frontend/src/assets/Adani_university_landing.png?raw=true",
+    className:
+      "md:row-start-1 md:row-end-3 md:col-start-3 md:col-end-5 col-start-1 col-end-2 row-start-1 row-end-3",
+  },
+  {
+    name: "Mindsurf",
+    description: "MindSurf is an adaptive learning platform that leverages AI to personalize education and enhance understanding of complex topics",
+    href: "https://github.com/aspdc/Janabs-HackSpire2025",
+    cta: "Learn more",
+    background: "https://github.com/aspdc/Janabs-HackSpire2025/blob/main/frontend/public/personalisedimage.jpg?raw=true",
+    className:
+      "md:col-start-4 md:col-end-5 md:row-start-3 md:row-end-5 col-start-1 col-end-3 row-start-4 row-end-5",
+  },
+  {
+    name: "TradeWise",
+    description:
+      "Stack: React, FastAPI, Angel One. A real-time stock market dashboard with live data, technical indicators, and portfolio management features.",
+    href: "https://github.com/P47Parzival/TradeWise",
+    cta: "Learn more",
+    background: "https://tse4.mm.bing.net/th/id/OIP.LWyAtEQ9lR_YuKWzcytHxQHaD0?rs=1&pid=ImgDetMain&o=7&rm=3",
+    className:
+      "md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-2  col-start-1 col-end-3 row-start-5 row-end-6 ",
+  },
+  {
+    name: "Custom Cursor Chrome Extension",
+    description: "Stack: HTML, CSS, JavaScript. Lets users pick custom cursors for Chrome.",
+    href: "https://github.com/P47Parzival/Custom-Cursor-Chrome-Extension",
+    cta: "Learn more",
+    background: "https://static3.makeuseofimages.com/wordpress/wp-content/uploads/2016/03/new-chrome-extensions.jpg",
+    className:
+      " md:col-start-1 md:col-end-3 md:row-start-2 md:row-end-3  col-start-1 col-end-2 row-start-3 row-end-3 ",
+  },
+  {
+    name: "House Prediction Production ready Pipeline",
+    description: "Linear, Ridge, Lasso,Random Forest, XGBoost",
+    href: "https://github.com/P47Parzival/production_ready_model_for_house_prediction",
+    cta: "Learn more",
+    background: "https://th.bing.com/th/id/OIP.3Psu7nDr2LvhWH4R6-TqegHaFj?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3",
+    className:
+      "md:row-start-1 md:row-end-2 md:col-start-5 md:col-end-8 col-start-1 col-end-2 row-start-1 row-end-3",
+  },
+  {
+    name: "LISS-4 Satellite Change Detection",
+    description:
+      "Stack: ML, CV, Python, Flask, GEE, Google Colab. Built for ISRO Hackathon 2025 — detects robust land changes from multi-temporal satellite imagery.",
+    href: "https://github.com/P47Parzival/LISS-4-Satellite-imagery",
+    cta: "Learn more",
+    background: "https://tse2.mm.bing.net/th/id/OIP.kEA4ZWdolAJzsTULJOnORgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3",
+    className:
+      "md:row-start-2 md:row-end-5 md:col-start-5 md:col-end-8 col-start-1 col-end-2 row-start-1 row-end-3",
+  },
+];
+
+export const skills = [
+  {
+    title: "Reactjs",
+    src: "/icons/1.svg",
+  },
+  {
+    title: "NodeJs",
+    src: "/icons/2.svg",
+  },
+  {
+    title: "Npm",
+    src: "/icons/3.svg",
+  },
+  {
+    title: "Nextjs",
+    src: "/icons/4.svg",
+  },
+  {
+    title: "Appwrite",
+    src: "/icons/5.svg",
+  },
+  {
+    title: "Expressjs",
+    src: "/icons/6.svg",
+  },
+  {
+    title: "MongoDB",
+    src: "/icons/7.svg",
+  },
+  {
+    title: "Vercel",
+    src: "/icons/8.svg",
+  },
+  {
+    title: "C++",
+    src: "/icons/9.svg",
+  },
+  {
+    title: "Javascript",
+    src: "/icons/10.svg",
+  },
+  {
+    title: "Typescript",
+    src: "/icons/11.svg",
+  },
+  {
+    title: "Html",
+    src: "/icons/12.svg",
+  },
+  {
+    title: "Css",
+    src: "/icons/13.svg",
+  },
+  {
+    title: "Bootstrap",
+    src: "/icons/14.svg",
+  },
+  {
+    title: "Tailwind",
+    src: "/icons/15.svg",
+  },
+  {
+    title: "Github",
+    src: "/icons/16.svg",
+  },
+  {
+    title: "SQL",
+    src: "/icons/17.svg",
+  },
+  {
+    title: "Docker",
+    src: "/icons/18.svg",
+  },
+];
+
+export const socials = {
+  github: 'https://github.com/P47Parzival',
+  twitter: 'https://x.com/dhruvm2804'
+};
